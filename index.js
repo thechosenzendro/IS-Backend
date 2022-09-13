d028818aa9e7784ce826ba51d3dcf8084696acfe
thechosenzendro <the.chosen.zendro@gmail.com>
2022-09-13 21:09:49 +0200

    Přidání requestu souboru

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
+//Inicializace všech potřebných modulů + proměnných
 const express = require("express");
 const app = express();
+const fs = require('fs');
 const port = 8080;
 app.listen(port, () => console.log("Vše funguje na localhost:" + port));
-
+//Middleware
 app.use(express.json());
 
-app.post("/auth", (req, res) => {
-    const { username } = req.body;
-    const { password } = req.body;
-    res.status(200).send({
-        message: "jj"
+//Request souboru z ./server
+app.get("/file/:filename", (req, res) => {
+    const { filename } = req.params
+    fs.readFile("./server/" + filename, "utf8", (err, data) => {
+        if (err) {
+            res.status(400).send(err);
+            console.log("Error: " + err)
+            return;
+        }
+        res.status(200).send(data)
+        console.log("Data poslána: " + filename)
+
     })
-    console.log("Username: " + username + ", Password: " + password)
 })
