70c32eafcf3028314dc68674a5a36d1fcde7e7df
thechosenzendro <the.chosen.zendro@gmail.com>
2022-09-13 21:37:51 +0200

    Přidáno CORS Policy

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 //Inicializace všech potřebných modulů + proměnných
-const express = require("express");
-const app = express();
-const fs = require('fs');
-const port = 8080;
+var express = require("express");
+var app = express();
+var fs = require('fs');
+var cors = require('cors')
+var port = 8080;
+var frontend = "http://127.0.0.1:5500"
 app.listen(port, () => console.log("Vše funguje na localhost:" + port));
 //Middleware
 app.use(express.json());
 
+//Nastavování pravidel CORS
+var corsOptions = {
+    origin: frontend,
+    optionsSuccessStatus: 200
+}
+
 //Request souboru z ./server
-app.get("/file/:filename", (req, res) => {
+app.get("/file/:filename", cors(corsOptions), (req, res) => {
     const { filename } = req.params
     fs.readFile("./server/" + filename, "utf8", (err, data) => {
         if (err) {
